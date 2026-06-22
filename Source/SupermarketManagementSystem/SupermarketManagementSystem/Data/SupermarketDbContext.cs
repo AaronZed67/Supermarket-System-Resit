25c37c0bebc001c66169254583cdda8951334ba9
AaronZed67 <pinta066@yahoo.com>
2026-06-22 19:15:29 +0100

    Database context added

---
+++ Source/SupermarketManagementSystem/SupermarketManagementSystem/Data/SupermarketDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SupermarketManagementSystem.Models;
+
+namespace SupermarketManagementSystem.Data
+{
+    public class SupermarketDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Stock> StockRecords { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+        public DbSet<SaleItem> SaleItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\MSSQLLocalDB;Database=SupermarketSystemResitDb;Trusted_Connection=True;");
+        }
+    }
+}
