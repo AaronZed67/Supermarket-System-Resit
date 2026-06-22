c423663c5e47e45deaf6e35f11a394c314c086df
AaronZed67 <pinta066@yahoo.com>
2026-06-22 17:35:17 +0100

    Stock Model

--- Source/SupermarketManagementSystem/SupermarketManagementSystem/Models/Stock.cs
+++ Source/SupermarketManagementSystem/SupermarketManagementSystem/Models/Stock.cs
@@ -1,1 +1,15 @@
-﻿
+﻿namespace SupermarketManagementSystem.Models
+{
+    public class Stock
+    {
+        public int StockId { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int QuantityAvailable { get; set; }
+
+        public DateTime LastUpdated { get; set; }
+
+        public DateTime RestockDate { get; set; }
+    }
+}
