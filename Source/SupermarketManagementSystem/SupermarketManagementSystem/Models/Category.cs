d8f5e9feace7e774c20c0cf9629bd5704dd9191f
AaronZed67 <pinta066@yahoo.com>
2026-06-22 18:42:43 +0100

    Category Model

--- Source/SupermarketManagementSystem/SupermarketManagementSystem/Models/Category.cs
+++ Source/SupermarketManagementSystem/SupermarketManagementSystem/Models/Category.cs
@@ -1,1 +1,8 @@
-﻿
+﻿namespace SupermarketManagementSystem.Models
+{
+    public class Category  // stores what category the product is in
+    {
+        public int CategoryId { get; set; }  //category identifier
+        public string CategoryName { get; set; } //category name for grouping
+    }
+}
