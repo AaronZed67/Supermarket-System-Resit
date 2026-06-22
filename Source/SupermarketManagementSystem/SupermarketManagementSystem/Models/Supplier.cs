b29cf66ac0c2c10cf184ea8df640b49b2dcc6a20
AaronZed67 <pinta066@yahoo.com>
2026-06-22 18:26:39 +0100

    Supplier model

--- Source/SupermarketManagementSystem/SupermarketManagementSystem/Models/Supplier.cs
+++ Source/SupermarketManagementSystem/SupermarketManagementSystem/Models/Supplier.cs
@@ -1,1 +1,10 @@
-﻿
+﻿namespace SupermarketManagementSystem.Models
+{
+    public class Supplier //stores suplier info
+    {
+        public int SupplierId { get; set; } //suplier identifier
+        public string SupplierName { get; set; }
+        public string ContactNumber { get; set; } //contact infor for communication withe suppliers
+
+    }
+}
