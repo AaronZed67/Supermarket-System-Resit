d9ccee315cb18d971b86b48aad01ce8018caa2aa
AaronZed67 <pinta066@yahoo.com>
2026-06-22 16:05:36 +0100

    Created console app project

---
+++ Source/SupermarketManagementSystem/SupermarketManagementSystem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SupermarketManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
