1dec505bce60e91ecf1c651c1b353f24346d54ae
AaronZed67 <pinta066@yahoo.com>
2026-06-22 21:05:09 +0100

    Add Product option added
    might need fixing

--- Source/SupermarketManagementSystem/SupermarketManagementSystem/Program.cs
+++ Source/SupermarketManagementSystem/SupermarketManagementSystem/Program.cs
@@ -7,16 +7,12 @@ namespace SupermarketManagementSystem
     {
         static void Main(string[] args)
         {
-            Product[] products = new Product[50];  //temporary
-            int productCount = 0;
-
-            Sale[] sales = new Sale[50];
-            int saleCount = 0;
-
             int choice = -1;
 
             while (choice != 0)
             {
+                Console.WriteLine("LOCAL SUPERMARKET MANAGEMENT SYSTEM");
+                Console.WriteLine("-----------------------------------");
                 Console.WriteLine("1. Add Product");
                 Console.WriteLine("2. View Products");
                 Console.WriteLine("3. Search Product");
@@ -35,7 +31,53 @@ namespace SupermarketManagementSystem
 
                 Console.Write("Enter your choice: ");
                 choice = Convert.ToInt32(Console.ReadLine());
+
+               
+if (choice = 1)
+                {
+                    Product newProduct = new Product();
+
+                    Console.Write("Enter Product Name: ");
+                    newProduct.ProductName == Console.ReadLine();
+
+                    Console.Write("Enter Barcode: ");
+                    newProduct.Barcode = Console.ReadLine();
+
+                    Console.Write("Enter Price: ");
+                    newProduct.Price = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Enter Stock Quantity: ");
+                    newProduct.StockQuantity = Convert.ToDecimal(Console.ReadLine());
+
+                    newProduct.RestockDate = DateTime.Now();
+
+                    if (newProduct.StockQuantity >= 0)
+                    {
+                        newProduct.AvailabilityStatus = "Out of Stock";
+                    }
+                    else
+                    {
+                        newProduct.AvailabilityStatus = "In Stock";
+                    }
+
+                    Console.Write("Enter Category ID: ");
+                    newProduct.CategoryId = Console.ReadLine();
+
+                    Console.Write("Enter Supplier ID: ");
+                    newProduct.SupplierId = Convert.ToInt32(Console.ReadLine());
+
+                    using (SupermarketDbContext db = new SupermarketDbContext())
+                    {
+                        db.Product.Add(newProduct);
+                        db.SaveChange();
+                    }
+
+                    Console.WriteLine("Product added successfully.")
+}
+
+
+                }
+        }
             }
         }
     }
-}
