f156a0714f3f1362cf7570852010a52879989d77
AaronZed67 <pinta066@yahoo.com>
2026-06-22 18:03:14 +0100

    Sale Model

--- Source/SupermarketManagementSystem/SupermarketManagementSystem/Models/Sale.cs
+++ Source/SupermarketManagementSystem/SupermarketManagementSystem/Models/Sale.cs
@@ -1,1 +1,13 @@
-﻿
+﻿namespace SupermarketManagementSystem.Models
+{
+
+    public class Sale // stores payment info
+    {
+
+        public int SaleId { get; set; } // sale id
+
+        public int ProductId { get; set; }
+        public int QuantitySold { get; set; } // how many products sold in the transaction
+        public DateTime SaleDate { get; set; }
+    }
+}
