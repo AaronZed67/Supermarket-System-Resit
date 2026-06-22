8c77e5c9118ae03674053b1d377b01fe39380360
AaronZed67 <pinta066@yahoo.com>
2026-06-22 16:52:43 +0100

    SaleItem Model

--- Source/SupermarketManagementSystem/SupermarketManagementSystem/Models/SaleItem.cs
+++ Source/SupermarketManagementSystem/SupermarketManagementSystem/Models/SaleItem.cs
@@ -1,1 +1,15 @@
-﻿
+﻿namespace SupermarketManagementSystem.Models
+{
+    public class SaleItem
+    {
+        public int SaleItemId { get; set; }
+
+        public int SaleId { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int QuantitySold { get; set; }
+
+        public decimal UnitPrice { get; set; }
+    }
+}
