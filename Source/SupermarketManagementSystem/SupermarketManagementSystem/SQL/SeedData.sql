dc41f88e2e7d6c9cddb541f37deaf9dcb525fec9
AaronZed67 <pinta066@yahoo.com>
2026-06-29 00:18:26 +0100

    category and supplier seed data added

---
+++ Source/SupermarketManagementSystem/SupermarketManagementSystem/SQL/SeedData.sql
@@ -0,0 +1,17 @@
+USE SupermarketSystemResitDb;
+GO
+
+INSERT INTO Categories (CategoryName)
+VALUES
+('Dairy'),
+('Bakery'),
+('Drinks'),
+('Fruit and Vegetables');
+GO
+
+INSERT INTO Suppliers (SupplierName, ContactNumber)
+VALUES
+('Fresh Foods Ltd', '07123456789'),
+('Daily Dairy Supplies', '07234567890'),
+('Bakery Wholesale Ltd', '07345678901');
+GO
