7f4fb813e0453cca0b09da1ea415b6af711f602c
AaronZed67 <pinta066@yahoo.com>
2026-06-29 19:06:34 +0100

    custom product list structure added

---
+++ Source/SupermarketManagementSystem/SupermarketManagementSystem/DataStructures/CustomProductList.cs
@@ -0,0 +1,30 @@
+﻿using SupermarketManagementSystem.Models
+
+namespace SupermarketManagementSystem.DataStructures
+{
+    public class CustomProductList
+    {
+        private Product[] products
+        private int count;
+
+        public CustomProductList()
+        {
+            products = new Product[100]
+            count = 0;
+        }
+
+        public void AddProduct(Product product)
+        {
+            if count < products.Length
+            {
+                products[count] = product;
+                count++;
+            }
+        }
+
+        public int GetCount()
+        {
+            return count
+        }
+    }
+}
