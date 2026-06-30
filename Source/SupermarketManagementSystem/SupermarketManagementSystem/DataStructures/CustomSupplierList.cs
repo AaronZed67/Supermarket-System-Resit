847fcbfc58f1dba6f200f3cc65ff868c1f11766d
AaronZed67 <pinta066@yahoo.com>
2026-06-30 16:47:52 +0100

    custom supplier list added

---
+++ Source/SupermarketManagementSystem/SupermarketManagementSystem/DataStructures/CustomSupplierList.cs
@@ -0,0 +1,56 @@
+﻿using SupermarketManagementSystem.Models;
+
+namespace SupermarketManagementSystem.DataStructures
+{
+    public class CustomSupplierList
+    {
+        private Supplier[] suppliers;
+        private int count;
+
+        public CustomSupplierList()
+        {
+            suppliers = new Supplier[100];
+            count = 0;
+        }
+
+        public void AddSupplier(Supplier supplier)
+        {
+            if (count < suppliers.Length)
+            {
+                suppliers[count] = supplier;
+                count++;
+            }
+        }
+
+        public Supplier SearchByName(string supplierName)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (suppliers[i].SupplierName == supplierName)
+                {
+                    return suppliers[i];
+                }
+            }
+
+            return null;
+        }
+
+        public Supplier SearchById(string supplierId)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (suppliers[i].SupplierId.ToString() == supplierId)
+                {
+                    return suppliers[i];
+                }
+            }
+
+            return null;
+        }
+
+        public int GetCount()
+        {
+            return count;
+        }
+    }
+}
