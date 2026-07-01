57fbddb1671d882f95adb0db9b7fbf8c62633cea
AaronZed67 <pinta066@yahoo.com>
2026-07-01 13:51:32 +0100

    low stock report unit tests
    all passed

--- Source/SupermarketManagementSystem/SupermarketManagementSystem.Tests/LowStockReportTests.cs
+++ Source/SupermarketManagementSystem/SupermarketManagementSystem.Tests/LowStockReportTests.cs
@@ -1,10 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SupermarketManagementSystem.Models;
 
 namespace SupermarketManagementSystem.Tests
 {
-    internal class LowStockReportTests
+    [TestClass]
+    public class LowStockReportTests
     {
+        [TestMethod]
+        public void LowStockReport_ProductWithFiveStockIsIncluded()
+        {
+            Product product = new Product();
+            product.ProductName = "Bread";
+            product.Barcode = "222";
+            product.StockQuantity = 5;
+
+            bool shouldAppearInLowStockReport = false;
+
+            if (product.StockQuantity <= 5)
+            {
+                shouldAppearInLowStockReport = true;
+            }
+
+            Assert.IsTrue(shouldAppearInLowStockReport);
+        }
+
+        [TestMethod]
+        public void LowStockReport_ProductWithZeroStockIsIncluded()
+        {
+            Product product = new Product();
+            product.ProductName = "Milk";
+            product.Barcode = "111";
+            product.StockQuantity = 0;
+
+            bool shouldAppearInLowStockReport = false;
+
+            if (product.StockQuantity <= 5)
+            {
+                shouldAppearInLowStockReport = true;
+            }
+
+            Assert.IsTrue(shouldAppearInLowStockReport);
+        }
+
+        [TestMethod]
+        public void LowStockReport_ProductWithMoreThanFiveStockIsNotIncluded()
+        {
+            Product product = new Product();
+            product.ProductName = "Orange Juice";
+            product.Barcode = "333";
+            product.StockQuantity = 10;
+
+            bool shouldAppearInLowStockReport = false;
+
+            if (product.StockQuantity <= 5)
+            {
+                shouldAppearInLowStockReport = true;
+            }
+
+            Assert.IsFalse(shouldAppearInLowStockReport);
+        }
+
+        [TestMethod]
+        public void LowStockReport_ProductWithNegativeStockIsIncluded()
+        {
+            Product product = new Product();
+            product.ProductName = "Error Product";
+            product.Barcode = "999";
+            product.StockQuantity = -1;
+
+            bool shouldAppearInLowStockReport = false;
+
+            if (product.StockQuantity <= 5)
+            {
+                shouldAppearInLowStockReport = true;
+            }
+
+            Assert.IsTrue(shouldAppearInLowStockReport);
+        }
     }
 }
